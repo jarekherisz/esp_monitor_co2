b60a1d3557348565041206869a1e97780c846d3c
WojciechHerisz <wojtek@herisz.pl>
2025-01-16 16:44:12 +0100

    added basic front-end for root endpoint and websockets to handle connection

--- firmware/esp_monitor_co2/esp_monitor_co2.ino
+++ firmware/esp_monitor_co2/esp_monitor_co2.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include <math.h>
 #include <Adafruit_GFX.h>
@@ -7,26 +6,25 @@
 #include <WiFiManager.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 
-
-
-#define SCREEN_WIDTH 128 // Szerokość ekranu OLED w pikselach
-#define SCREEN_HEIGHT 64 // Wysokość ekranu OLED w pikselach
-#define OLED_RESET -1    // Reset pin nie jest używany
+#define SCREEN_WIDTH 128  // Szerokość ekranu OLED w pikselach
+#define SCREEN_HEIGHT 64  // Wysokość ekranu OLED w pikselach
+#define OLED_RESET -1     // Reset pin nie jest używany
 #define OLED_ADDRESS 0x3C // Adres I2C wyświetlacza OLED
-#define BAUD_RATE 9600   // Prędkość transmisji danych przez port szeregowy
-
+#define BAUD_RATE 9600    // Prędkość transmisji danych przez port szeregowy
 
 int ppmHistory[SCREEN_WIDTH] = {0}; // Tablica dla historii pomiarów
-int historyIndex = 0; // Aktualny indeks w historii
+int historyIndex = 0;               // Aktualny indeks w historii
 const byte DNS_PORT = 53;
 
 DNSServer dnsServer;
 
 // Inicjalizacja serwera na porcie 80
 ESP8266WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
 
-//Zmienne do liczenie kiedy odpytac sensor
+// Zmienne do liczenie kiedy odpytac sensor
 unsigned long lastMeasurementTime = 0;
 const unsigned long measurementInterval = 30000;
 
@@ -38,17 +36,19 @@ WiFiManager wm;
 SCD4x mySensor;
 
 int currentPpm = 0;
-float  currentTemperature = 0;
-float  currentHumidity = 0;
+float currentTemperature = 0;
+float currentHumidity = 0;
 
 void setupDisplay()
 {
   // Inicjalizacja wyświetlacza OLED
-  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) { // Używanie stałej OLED_ADDRESS
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS))
+  { // Używanie stałej OLED_ADDRESS
     Serial.println(F("Nie można zainicjować SSD1306"));
-    for (;;); // Zatrzymanie działania programu, jeśli nie można zainicjować wyświetlacza
+    for (;;)
+      ; // Zatrzymanie działania programu, jeśli nie można zainicjować wyświetlacza
   }
-  
+
   display.clearDisplay();
   display.setTextSize(1);      // Ustawienie rozmiaru tekstu
   display.setTextColor(WHITE); // Ustawienie koloru tekstu
@@ -66,7 +66,7 @@ void setupDisplay()
   display.println(OLED_ADDRESS);
   display.print("BAUD_RATE: ");
   display.println(BAUD_RATE);
-  display.display();           // Odświeżenie wyświetlacza
+  display.display(); // Odświeżenie wyświetlacza
 
   // Wyświetlanie wszystkich zdefiniowanych stałych w konsoli COM
   Serial.println("Zdefiniowane stałe:");
@@ -87,90 +87,95 @@ void setupDisplay()
 void setupWiFi()
 {
 
-    String SSID = "Sensor Co2";
-    wm.setConfigPortalBlocking(false);
-    
-    bool res = wm.autoConnect(SSID.c_str());
+  String SSID = "Sensor Co2";
+  wm.setConfigPortalBlocking(false);
 
-    if (!res) {
-        Serial.println("Failed to connect and hit timeout");
-        // Możesz zrestartować ESP, jeśli połączenie nie powiedzie się
-        // ESP.restart();
-    } else {
-        // Informacja o pomyślnym połączeniu z WiFi
-        Serial.println("connected...yeey :)");
-        
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        display.println("Settings:");
-        
-        // Wyświetlenie szczegółów połączenia
-        Serial.print("IP Address: ");
-        Serial.println(WiFi.localIP()); // Adres IP przydzielony ESP
-        display.print("IP: ");
-        display.println(WiFi.localIP());
-        
+  bool res = wm.autoConnect(SSID.c_str());
 
-        Serial.print("Subnet Mask: ");
-        Serial.println(WiFi.subnetMask()); // Maska podsieci
-        display.print("M: ");
-        display.println(WiFi.subnetMask());
+  if (!res)
+  {
+    Serial.println("Failed to connect and hit timeout");
+    // Możesz zrestartować ESP, jeśli połączenie nie powiedzie się
+    // ESP.restart();
+  }
+  else
+  {
+    // Informacja o pomyślnym połączeniu z WiFi
+    Serial.println("connected...yeey :)");
 
-        Serial.print("Gateway IP: ");
-        Serial.println(WiFi.gatewayIP()); // Adres IP bramy domyślnej
-        display.print("G: ");
-        display.println(WiFi.gatewayIP());
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Settings:");
 
-        Serial.print("DNS Server: ");
-        Serial.println(WiFi.dnsIP()); // Adres IP serwera DNS
-        display.print("D: ");
-        display.println(WiFi.dnsIP());
+    // Wyświetlenie szczegółów połączenia
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP()); // Adres IP przydzielony ESP
+    display.print("IP: ");
+    display.println(WiFi.localIP());
 
-        display.display();           // Odświeżenie wyświetlacza
+    Serial.print("Subnet Mask: ");
+    Serial.println(WiFi.subnetMask()); // Maska podsieci
+    display.print("M: ");
+    display.println(WiFi.subnetMask());
 
-        delay(5000);
-    }
+    Serial.print("Gateway IP: ");
+    Serial.println(WiFi.gatewayIP()); // Adres IP bramy domyślnej
+    display.print("G: ");
+    display.println(WiFi.gatewayIP());
 
-     //Wyswietla informacje o trybie konfiguracji
-     if (WiFi.status() != WL_CONNECTED) {
-        display.clearDisplay();
-        display.setCursor(0, 0);
-        
-        // Wyświetlenie szczegółów połączenia
-        Serial.print("IP Address: ");
-        Serial.println(WiFi.softAPIP()); // Adres IP przydzielony ESP
-        display.print("IP: ");
-        display.println(WiFi.softAPIP());
+    Serial.print("DNS Server: ");
+    Serial.println(WiFi.dnsIP()); // Adres IP serwera DNS
+    display.print("D: ");
+    display.println(WiFi.dnsIP());
 
-        display.print("SSID: ");
-        display.println(SSID);
+    display.display(); // Odświeżenie wyświetlacza
 
-        display.display();           // Odświeżenie wyświetlacza
+    delay(5000);
+  }
 
-        delay(5000);
-     }
+  // Wyswietla informacje o trybie konfiguracji
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    display.clearDisplay();
+    display.setCursor(0, 0);
+
+    // Wyświetlenie szczegółów połączenia
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.softAPIP()); // Adres IP przydzielony ESP
+    display.print("IP: ");
+    display.println(WiFi.softAPIP());
+
+    display.print("SSID: ");
+    display.println(SSID);
+
+    display.display(); // Odświeżenie wyświetlacza
+
+    delay(5000);
+  }
 }
 
 void setupSCD4x()
 {
   Wire.begin(D2, D1); // Inicjalizacja I2C z D2 jako SDA i D1 jako SCL
 
-  if (mySensor.begin() == false) {
+  if (mySensor.begin() == false)
+  {
     Serial.println("Sensor not detected. Please check wiring. Freezing...");
-    while (1); // Zawieszenie programu, jeśli czujnik nie zostanie wykryty
+    while (1)
+      ; // Zawieszenie programu, jeśli czujnik nie zostanie wykryty
   }
 
   // Najpierw musimy zatrzymać okresowe pomiary, w przeciwnym razie uruchomienie pomiarów okresowych o niskim zużyciu energii nie powiedzie się
   if (mySensor.stopPeriodicMeasurement() == true)
   {
     Serial.println(F("Okresowe pomiary zostały wyłączone!"));
-  }  
+  }
 
   // Teraz możemy włączyć okresowe pomiary o niskim zużyciu energii
   if (mySensor.startLowPowerPeriodicMeasurement() == true)
   {
     Serial.println(F("Włączono tryb niskiego zużycia energii!"));
-  }  
+  }
 
   Serial.println("SCD41 sensor initialized and measurement started!");
 
@@ -178,36 +183,42 @@ void setupSCD4x()
   lastMeasurementTime = millis() - measurementInterval;
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(BAUD_RATE); // Rozpoczynanie komunikacji szeregowej z prędkością 9600 bps
-
-
 
   setupDisplay();
   setupSCD4x();
   setupWiFi();
 
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     // Ustawienie ścieżki dla URI
+    server.on("/", handleRoot);
     server.on("/currentData", handleCurrentData);
     server.on("/factoryReset", handleFactoryReset);
     server.begin();
     Serial.println("HTTP server started.");
-  }
 
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+    Serial.println("WebSocket server started.");
+  }
 }
 
 // Funkcja do rysowania wykresu
-void drawPpmGraph() {
-  int graphHeight = 40; // Wysokość wykresu (dolna część ekranu)
+void drawPpmGraph()
+{
+  int graphHeight = 40;                     // Wysokość wykresu (dolna część ekranu)
   int graphY = SCREEN_HEIGHT - graphHeight; // Pozycja Y wykresu (dolny margines)
 
   // Obliczanie zakresu wartości
   int maxPpm = 2000; // Maksymalna wartość CO2 do wizualizacji
   int minPpm = 400;  // Minimalna wartość CO2 do wizualizacji
 
-  for (int i = 0; i < SCREEN_WIDTH; i++) {
-   
+  for (int i = 0; i < SCREEN_WIDTH; i++)
+  {
+
     // Mapowanie wartości ppm na wysokość wykresu
     int barHeight = map(ppmHistory[i], minPpm, maxPpm, 0, graphHeight);
     barHeight = constrain(barHeight, 0, graphHeight); // Ograniczenie do wysokości wykresu
@@ -218,14 +229,19 @@ void drawPpmGraph() {
 }
 
 // Funkcja do dodawania nowych wartości do historii
-void addToPpmHistory(int ppmValue) {
+void addToPpmHistory(int ppmValue)
+{
   // Sprawdź, czy indeks osiągnął koniec tablicy
-  if (historyIndex < SCREEN_WIDTH) {
+  if (historyIndex < SCREEN_WIDTH)
+  {
     // Dodaj wartość na obecnym indeksie
     ppmHistory[historyIndex++] = ppmValue;
-  } else {
+  }
+  else
+  {
     // Przesunięcie wszystkich elementów o jeden w lewo
-    for (int i = 1; i < SCREEN_WIDTH; i++) {
+    for (int i = 1; i < SCREEN_WIDTH; i++)
+    {
       ppmHistory[i - 1] = ppmHistory[i];
     }
     // Dodaj nową wartość na końcu tablicy
@@ -233,61 +249,68 @@ void addToPpmHistory(int ppmValue) {
   }
 }
 
-
 void loop()
 {
-   if (WiFi.status() == WL_CONNECTED) {
-        loopSennsorMode();
-    } else {
-        loopConfigMode();
-    }
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    loopSennsorMode();
+  }
+  else
+  {
+    loopConfigMode();
+  }
 }
 
 void displaySensorData()
 {
-    Serial.println("CO2: " + String(currentPpm) + " ppm");
-    Serial.println("temperature: " + String(currentTemperature, 1) + "C");
-    Serial.println("humidity: " + String(currentHumidity, 1) + "%");
+  Serial.println("CO2: " + String(currentPpm) + " ppm");
+  Serial.println("temperature: " + String(currentTemperature, 1) + "C");
+  Serial.println("humidity: " + String(currentHumidity, 1) + "%");
 
-    // Ustwianie OLED
-    display.clearDisplay();
-    display.setTextSize(2); // Ustawienie większego rozmiaru tekstu dla kluczowych danych
+  // Ustwianie OLED
+  display.clearDisplay();
+  display.setTextSize(2); // Ustawienie większego rozmiaru tekstu dla kluczowych danych
 
-    // Wyśrodkowanie poziomu CO2
-    String co2Text = String(currentPpm) + "ppm";
-    int16_t x1, y1;
-    uint16_t textWidth, textHeight;
-    display.getTextBounds(co2Text, 0, 0, &x1, &y1, &textWidth, &textHeight);
-    int co2X = (SCREEN_WIDTH - textWidth) / 2;
-    display.setCursor(co2X, 0);
-    display.println(co2Text);
+  // Wyśrodkowanie poziomu CO2
+  String co2Text = String(currentPpm) + "ppm";
+  int16_t x1, y1;
+  uint16_t textWidth, textHeight;
+  display.getTextBounds(co2Text, 0, 0, &x1, &y1, &textWidth, &textHeight);
+  int co2X = (SCREEN_WIDTH - textWidth) / 2;
+  display.setCursor(co2X, 0);
+  display.println(co2Text);
 
-    // Wyśrodkowanie temperatury lub wilgotności
-    String dataText;
-    dataText = String(currentTemperature, 0) + "C " + String(currentHumidity, 0) + "%";
-    display.getTextBounds(dataText, 0, 0, &x1, &y1, &textWidth, &textHeight);
-    int dataX = (SCREEN_WIDTH - textWidth) / 2;
-    display.setCursor(dataX, 18); // Wyśrodkowanie w pionie
-    display.print(dataText);
+  // Wyśrodkowanie temperatury lub wilgotności
+  String dataText;
+  dataText = String(currentTemperature, 0) + "C " + String(currentHumidity, 0) + "%";
+  display.getTextBounds(dataText, 0, 0, &x1, &y1, &textWidth, &textHeight);
+  int dataX = (SCREEN_WIDTH - textWidth) / 2;
+  display.setCursor(dataX, 18); // Wyśrodkowanie w pionie
+  display.print(dataText);
 
-    // Rysowanie wykresu
-    drawPpmGraph();
+  // Rysowanie wykresu
+  drawPpmGraph();
 
-    display.display(); // Odświeżenie wyświetlacza
+  display.display(); // Odświeżenie wyświetlacza
 }
 
 void readSennsor()
 {
-  //Sensor odpytuje co measurementInterval milisekund, inaczej sie grzeje
-  if (millis() - lastMeasurementTime >= measurementInterval) {
+  // Sensor odpytuje co measurementInterval milisekund, inaczej sie grzeje
+  if (millis() - lastMeasurementTime >= measurementInterval)
+  {
     lastMeasurementTime = millis();
-    if (mySensor.readMeasurement()) {
+    if (mySensor.readMeasurement())
+    {
       currentPpm = mySensor.getCO2();
       currentTemperature = mySensor.getTemperature();
       currentHumidity = mySensor.getHumidity();
-  
+
       addToPpmHistory(currentPpm); // Dodaj bieżący ppm do historii
-  
+      String json = "{\"CO2\": " + String(currentPpm) +
+                    ", \"Temperature\": " + String(currentTemperature) +
+                    ", \"Humidity\": " + String(currentHumidity) + "}";
+      webSocket.broadcastTXT(json);
       displaySensorData();
     }
   }
@@ -299,20 +322,88 @@ void loopConfigMode()
   wm.process();
 }
 
-void loopSennsorMode() {
+void loopSennsorMode()
+{
   readSennsor();
   server.handleClient();
+  webSocket.loop();
 }
 
 // Funkcja obsługująca żądanie HTTP
-void handleCurrentData() {
-  String json = "{\"CO2\": " + String(currentPpm) + 
-                ", \"Temperature\": " + String(currentTemperature) + 
+void handleCurrentData()
+{
+  String json = "{\"CO2\": " + String(currentPpm) +
+                ", \"Temperature\": " + String(currentTemperature) +
                 ", \"Humidity\": " + String(currentHumidity) + "}";
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.sendHeader("Access-Control-Max-Age", "10000");
+  server.sendHeader("Access-Control-Allow-Methods", "PUT,POST,GET,OPTIONS");
+  server.sendHeader("Access-Control-Allow-Headers", "*");
   server.send(200, "application/json", json);
 }
 
-void handleFactoryReset() {
-  WiFi.disconnect(true);  // Usuwa dane sieci Wi-Fi, czyści konfigurację
-  ESP.restart();          // Restartuje mikrokontroler
+void handleFactoryReset()
+{
+  WiFi.disconnect(true); // Usuwa dane sieci Wi-Fi, czyści konfigurację
+  ESP.restart();         // Restartuje mikrokontroler
+}
+
+void handleRoot()
+{
+  String html = R"(
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>CO2 Monitor</title>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+          background-color: #f4f4f9;
+        }
+        h1 {
+          color: #333;
+        }
+        #data {
+          background: #fff;
+          padding: 20px;
+          border-radius: 8px;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          text-align: center;
+        }
+      </style>
+    </head>
+    <body>
+      <div id='data'>
+        <h1>CO2 Monitor Data</h1>
+        <div id='values'></div>
+      </div>
+      <script>
+        const ws = new WebSocket(`ws://${window.location.hostname}:81/`);
+        ws.onmessage = function(event) {
+          const data = JSON.parse(event.data);
+          document.getElementById('values').innerHTML = `CO2: ${data.CO2} ppm<br>Temperature: ${data.Temperature} *C<br>Humidity: ${data.Humidity} %`;
+        };
+      </script>
+    </body>
+    </html>
+  )";
+
+  server.send(200, "text/html", html);
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  if (type == WStype_CONNECTED)
+  {
+    Serial.printf("[%u] Connected\n", num);
+  }
+  else if (type == WStype_DISCONNECTED)
+  {
+    Serial.printf("[%u] Disconnected\n", num);
+  }
 }
